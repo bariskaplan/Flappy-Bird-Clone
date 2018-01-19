316b176c0933d3b7385e30bb8fb9654c5704d998
BARIŞ KAPLAN <bariskaplan1903@hotmail.com>
2018-01-19 10:34:48 +0300

    before merge

---
+++ FlappyBird/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rigidBody2d;
+
+	public float jumpRate = 5f;
+
+	void Start () {
+		rigidBody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+			rigidBody2d.AddForce(Vector2.up * jumpRate);
+		}
+	}
+}
